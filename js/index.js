d68adbda6531594e08bee48898d4b3c7b233486c
ige33 <ige33@YJ-BOOK>
2023-05-31 04:08:00 +0900

    프로젝트 페이지 추가, 페이지 이동 시에도 테마 유지

--- js/index.js
+++ js/index.js
@@ -15,57 +15,67 @@ function scrollToBottom() {
 
 
 // 다크,라이트모드 관련
+
+// 전체삭제
+// window.localStorage.clear();
+
+const $body = document.querySelector('body');
+const icons = document.querySelectorAll('.icon-img');
 const switchBtn = document.querySelector('#switchBtn');
 
-if (switchBtn.getAttribute('data-status') == 'light') {
-    document.querySelector('#utterance-dark').style.display = 'none';
-    switchBtn.innerHTML = 'dark';
-} else {
+// 페이지가 열렸을 때의 테마 확인
+let theme = window.localStorage.getItem('theme');
+
+// 다크모드 function
+const darkModeFunc = function() {
+    // 색상 코드 변경
+    $body.style.setProperty('--bg', '#222');
+    $body.style.setProperty('--font', '#fff');
+    $body.style.setProperty('--border', '#484848');
+
+    // 아이콘 변경
+    icons.forEach(function(icon) {
+        icon.src = icon.src.replace('fff', '222');
+    });
+
+    // 댓글창 변경
     document.querySelector('#utterance-light').style.display = 'none';
+    document.querySelector('#utterance-dark').style.display = 'block';
+
+    // 상태는 다크모드로 바꾸고 라이트 모드로 변경하는 버튼 노출
+    window.localStorage.setItem('theme', 'dark');
+    theme =  window.localStorage.getItem('theme');
     switchBtn.innerHTML = 'light';
 }
 
+// 라이트모드 function
+const lightModeFunc = function() {
+    $body.style.setProperty('--bg', '#fff');
+    $body.style.setProperty('--font', '#222');
+    $body.style.setProperty('--border', '#efefef');
+
+    icons.forEach(function(icon) {
+        icon.src = icon.src.replace('222', 'fff');
+    });
+
+    document.querySelector('#utterance-light').style.display = 'block';
+    document.querySelector('#utterance-dark').style.display = 'none';
+
+    window.localStorage.setItem('theme', 'light');
+    theme =  window.localStorage.getItem('theme');
+    switchBtn.innerHTML = 'dark';
+}
+
+// 페이지가 열렸을 때, 만약 localStorage에서 가져온 theme값이 dark라면 다크모드
+if (theme == 'dark') {
+    darkModeFunc();
+}
+
+// 버튼을 클릭했을 때, 현재 테마가 라이트라면 다크로 반대는 반대로~
 switchBtn.addEventListener('click', function(e) {
-
-    const $body = document.querySelector('body');
-    const dataStatus = this.getAttribute('data-status');
-    const icons = document.querySelectorAll('.icon-img');
-
-    // 라이트 모드라면 다크 모드로
-    if (dataStatus == 'light') {
-
-        // 색상 코드 변경
-        $body.style.setProperty('--bg', '#222');
-        $body.style.setProperty('--font', '#fff');
-        $body.style.setProperty('--border', '#484848');
-
-        // 아이콘 변경
-        icons.forEach(function(icon) {
-            icon.src = icon.src.replace('fff', '222');
-        });
-
-        // 댓글창 변경
-        document.querySelector('#utterance-light').style.display = 'none';
-        document.querySelector('#utterance-dark').style.display = 'block';
-
-        // 상태는 다크모드로 바꾸고 라이트 모드로 변경하는 버튼 노출
-        this.setAttribute('data-status', 'dark');
-        this.innerHTML = 'light';
-
+    if (theme == 'light') {
+        darkModeFunc();
     } else {
-
-        $body.style.setProperty('--bg', '#fff');
-        $body.style.setProperty('--font', '#222');
-        $body.style.setProperty('--border', '#efefef');
-
-        icons.forEach(function(icon) {
-            icon.src = icon.src.replace('222', 'fff');
-        });
-
-        document.querySelector('#utterance-light').style.display = 'block';
-        document.querySelector('#utterance-dark').style.display = 'none';
-
-        this.setAttribute('data-status', 'light');
-        this.innerHTML = 'dark';
+        lightModeFunc();
     }
 });
